77e41fb90fb902b74c9bc98b423058c1c0aed70c
zewenwang <zewenwang.michael@gmail.com>
2018-01-25 23:28:17 +0800

    kotlin support

---
+++ app/src/main/java/com/skeeter/leetcode/TestJava.java
@@ -0,0 +1,7 @@
+package com.skeeter.leetcode;
+
+/**
+ * @author zewenwang created on 2018/1/25.
+ */
+public class TestJava {
+}
---
+++ app/src/main/java/com/skeeter/leetcode/TestKotlin.kt
@@ -0,0 +1,13 @@
+package com.skeeter.leetcode
+
+/**
+ *
+ * @author  zewenwang created on 2018/1/25.
+ */
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
+
+class TestKotlin {
+
+}
