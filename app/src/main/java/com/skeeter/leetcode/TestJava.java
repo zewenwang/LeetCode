a8c05928c9271e8aec0312a180b2d69e73541faa
zewenwang <zewenwang.michael@gmail.com>
2018-01-26 14:18:56 +0800

    test

--- app/src/main/java/com/skeeter/leetcode/TestJava.java
+++ app/src/main/java/com/skeeter/leetcode/TestJava.java
@@ -4,4 +4,7 @@ package com.skeeter.leetcode;
  * @author zewenwang created on 2018/1/25.
  */
 public class TestJava {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
--- app/src/main/java/com/skeeter/leetcode/TestKotlin.kt
+++ app/src/main/java/com/skeeter/leetcode/TestKotlin.kt
@@ -9,5 +9,4 @@ fun main(args: Array<String>) {
 }
 
 class TestKotlin {
-
 }
