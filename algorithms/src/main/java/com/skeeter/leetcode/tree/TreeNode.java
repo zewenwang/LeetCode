4c5ba65aed03163d75ffd885ae9b8b810cb4485c
zewenwang <zewenwang.michael@gmail.com>
2018-02-01 23:14:30 +0800

    树节点

---
+++ algorithms/src/main/java/com/skeeter/leetcode/tree/TreeNode.java
@@ -0,0 +1,12 @@
+package com.skeeter.leetcode.tree;
+
+/**
+ * @author zewenwang created on 2018/1/30.
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) { val = x; }
+}
